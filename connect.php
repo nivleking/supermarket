b2d30642152115caf04d731d12c754f7a7adc7ef
leonardo <nickholasleonardo@gmail.com>
2024-05-30 23:25:01 +0700

    feat: add gitignore

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "supermarket";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
