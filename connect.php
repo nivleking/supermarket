3895429aad0754be2a986d9fec36dfed2ab0785b
Kelvin Sidharta Sie <118882421+nivleking@users.noreply.github.com>
2024-05-31 10:13:47 +0700

    feat: json and xlsx dataset

--- connect.php
+++ connect.php
@@ -7,9 +7,9 @@ $username = "root";
 $password = "root";
 $dbname = "supermarket";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 ?>
